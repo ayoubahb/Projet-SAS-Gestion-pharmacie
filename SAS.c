ebec9da263513b3f6457d4553324af3a2622ee87
ayoubahb <ayoubahabbane@gmail.com>
2022-09-19 20:33:54 +0100

    19/9

--- SAS.c
+++ SAS.c
@@ -3,11 +3,20 @@
 #include <string.h>
 #include <stdbool.h>
 
+
+int position_code();
 void affichage ();
+void ajout();
+void tri();
+void achat();
+void recherche();
+void etat();
+void alimentation();
+void supprime();
 //Global variable
 
 int nbr_prd = 5 ;
-
+float prix_total = 0;
 //Structure
 
 typedef struct produit
@@ -17,47 +26,58 @@ typedef struct produit
   int quantite;
   float prix;
   float prix_ttc;
+  int quantite_vendu;
 }PRODUIT;
 
 /*====================================Fonctions====================================*/
-// int position_code(PRODUIT arr[],char prd_ach[50]){
-//   int cible = -1;
-//   for (int i = 0; i < nbr_prd; i++)
-//   {
-//     if(strcmp(arr[i].code,prd_ach)==0){
-//       cible = i;
-//       break;
-//     }
-//   }
-//   return cible;
-// }
-//ajouter un ou plusieur produit 
-// void ajout(PRODUIT arr[],int n){
-//   int j=1;
-//   for (int i = nbr_prd; i < n+nbr_prd; i++)
-//   {
-//     // fflush(stdin);
-//     printf("======== Produit %d ========",j);
-//     printf("\ncode :");
-//     fgets(arr[i].code,50,stdin);
-//     printf("\nnom :");
-//     fgets(arr[i].nom,50,stdin);
-//     printf("\nqty :");
-//     scanf("%d",&arr[i].quantite);
-//     printf("\nprix :");
-//     scanf("%f",&arr[i].prix);
-//     fflush(stdin);
-//     arr[i].prix_ttc = arr[i].prix+(arr[i].prix * 0.15);
-//     j++;
-//   }
-//   nbr_prd+=n;
-//   printf("Produit(s) ajoute avec succsse");
-// }
+int position_code(PRODUIT arr[],char prd_ach[50]){
+  int cible = -1;
+  for (int i = 0; i < nbr_prd; i++)
+  {
+    if(strcmp(arr[i].code,prd_ach)==0){
+      cible = i;
+      break;
+    }
+  }
+  return cible;
+}
+// ajouter un ou plusieur produit 
+void ajout(PRODUIT arr[]){
+  int n ;
+  int j=1;
+  printf("\nCombient de produit vous pouvez ajouter : ");
+  scanf("%d",&n);
+  fflush(stdin); 
+  for (int i = nbr_prd; i < n+nbr_prd; i++)
+  {
+    printf("======== Produit %d ========",j);
+    printf("\ncode :");
+    fgets(arr[i].code,50,stdin);
+    printf("\nnom :");
+    fgets(arr[i].nom,50,stdin);
+    printf("\nqty :");
+    scanf("%d",&arr[i].quantite);
+    printf("\nprix :");
+    scanf("%f",&arr[i].prix);
+    fflush(stdin);
+    arr[i].prix_ttc = arr[i].prix+(arr[i].prix * 0.15);
+    arr[i].quantite_vendu=0;
+    j++;
+  }
+  nbr_prd+=n;
+  printf("Produit(s) ajoute avec succsse");
+}
 //trier la list principal avec les prix des produit **order decroissant** ou nom **order alphabetique
-void tri (PRODUIT arr[],int n){
-  int index;
+void tri (PRODUIT arr[]){
+  int index,ord;
   PRODUIT temp;
-  if (n==1)
+  printf("\nDe quel ordre vous pouvez afficher la list des produit \n");
+  printf("\n1.Par prix ou prix TTC(order decroissant)");
+  printf("\n2.Par order alphabetique");
+  printf("\n0.Retour a la menu");
+  printf("\n\nVotre choix : ");
+  scanf("%d",&ord);
+  if (ord==1)
   {
     for (int i = 0; i < nbr_prd-1; i++)
     {
@@ -76,7 +96,7 @@ void tri (PRODUIT arr[],int n){
       }
     }
     affichage (arr);
-  }else if (n==2)
+  }else if (ord==2)
   {
     for (int i = 0; i < nbr_prd-1; i++)
     {
@@ -95,6 +115,8 @@ void tri (PRODUIT arr[],int n){
       }
     }
     affichage (arr);
+  }else if(ord==0){
+    printf("\n");
   }else{
     printf("\nCe choix n'excist pas.");
   }
@@ -112,21 +134,24 @@ void affichage (PRODUIT arr[]){
   }
 }
 //Achat de produit
-void achat(PRODUIT arr[],char prd_ach[50],int qty){
-  int cible = -1;
-  for (int i = 0; i < nbr_prd; i++)
-  {
-    if(strcmp(arr[i].code,prd_ach)==0){
-      cible = i;
-      break;
-    }
-  }
+void achat(PRODUIT arr[]){
+  char nom_prd[50];
+  int qty_ach;
+  fflush(stdin);    
+  printf("Veuillez entrer le code de produit a aheter : ");
+  fgets(nom_prd,50,stdin);
+  int cible;
+  cible = position_code(arr,nom_prd);
   if(cible!=-1){
     if (arr[cible].quantite == 0)
     {
       printf("\nEn repture de stock\n");
     }else{
-      arr[cible].quantite -= qty;
+      printf("\nLa quantite a aheter : ");
+      scanf("%d",&qty_ach);
+      arr[cible].quantite -= qty_ach;
+      arr[cible].quantite_vendu += qty_ach;
+      prix_total += arr[cible].quantite_vendu * arr[cible].prix;
       printf("\nVotre achat a ete effectue\n");
     }
   }else{
@@ -134,21 +159,23 @@ void achat(PRODUIT arr[],char prd_ach[50],int qty){
   }
 }
 //chercher et afficher le produit
-void recherche(PRODUIT arr[],int n){
-  int cible = -1;
+void recherche(PRODUIT arr[]){
+  int n;
+  int cible;
+
+  printf("\nDe quel recherche vous pouvez chercher \n");
+  printf("\n1.Par code");
+  printf("\n2.Par quantite");
+  printf("\n0.Retour a la menu");
+  printf("\n\nVotre choix : ");
+  scanf("%d",&n);
+  
   if (n==1){
     fflush(stdin);
     char prd_ach[50];
     printf("Entrer le code de produit : ");
     fgets(prd_ach,50,stdin);
-    // fflush(stdin);
-    for (int i = 0; i < nbr_prd; i++)
-    {
-      if(strcmp(arr[i].code,prd_ach)==0){
-        cible = i;
-        break;
-      }
-    }
+    cible = position_code(arr,prd_ach);
     if(cible!=-1){
       printf("\n\ncode : %s",arr[cible].code);
       printf("nom : %s",arr[cible].nom);
@@ -179,9 +206,13 @@ void recherche(PRODUIT arr[],int n){
       printf("\nAucun produit avec la quantite entree dans le stock\n");
     }
 
-  }else{
+  }else if(n==0){
+    printf("\n");
+  }else
+  {
     printf("\nCe choix n'excist pas.");
   }
+  
 }
 //afficher les produits dont leur etate de stock inferieur de 3
 void etat(PRODUIT arr[]){
@@ -203,16 +234,10 @@ void etat(PRODUIT arr[]){
     printf("Pas de produit dont la quantite inferieur a 3");
   }
 }
-//incrementer la quantite d'0n produit
+//incrementer la quantite d'un produit
 void alimentation(PRODUIT arr[],char prd_ach[50],int qty){
-  int cible = -1;
-  for (int i = 0; i < nbr_prd; i++)
-  {
-    if(strcmp(arr[i].code,prd_ach)==0){
-      cible = i;
-      break;
-    }
-  }
+  int cible;
+  cible = position_code(arr,prd_ach);
   if(cible!=-1){
     arr[cible].quantite += qty;
     printf("\nVotre alimentation a ete effectue\n");
@@ -222,14 +247,8 @@ void alimentation(PRODUIT arr[],char prd_ach[50],int qty){
 }
 //supprimer un produit
 void supprime (PRODUIT arr[],char prd_ach[50]){
-  int cible = -1;
-  for (int i = 0; i < nbr_prd; i++)
-  {
-    if(strcmp(arr[i].code,prd_ach)==0){
-      cible = i;
-      break;
-    }
-  }
+  int cible;
+  cible = position_code(arr,prd_ach);
   for (int i = cible; i < nbr_prd; i++)
   {
     arr[i]=arr[i+1];
@@ -239,9 +258,8 @@ void supprime (PRODUIT arr[],char prd_ach[50]){
 
 int main(){
   //Variables
-  int ch,ord,qty_ach,rch;
+  int ch;
   int qty_al;
-  int nbr_ajout;
 
   char nom_prd[50];
   PRODUIT prd[1000];
@@ -287,7 +305,7 @@ int main(){
   {
     fflush(stdin);
     printf("\n\n--------- Bienvenue --------- \n\n");
-    // printf("1.Ajouter des produits");
+    printf("1.Ajouter des produits");
     printf("\n2.Afficher les produits");
     printf("\n3.Acheter un produit");
     printf("\n4.Chercher un produit avec code");
@@ -298,47 +316,20 @@ int main(){
     scanf("%d",&ch);
     switch (ch)
     {
-      case 1:
-        // system("cls");
-        printf("\nCombient de produit vous pouvez ajouter : ");
-        scanf("%d",&nbr_ajout);
-        fflush(stdin);    
-        // ajout(prd,nbr_ajout);
+      case 1:    
+        ajout(prd);
         break;
       
       case 2:
-        printf("\nDe quel ordre vous pouvez afficher la list des produit \n");
-        printf("\n1.Par prix ou prix TTC(order decroissant)");
-        printf("\n2.Par order alphabetique");
-        printf("\n0.Retour a la menu");
-        do
-        {
-          printf("\n\nVotre choix : ");
-          scanf("%d",&ord);
-        } while (ord<0 && ord>3);
-        tri(prd,ord);
+        tri(prd);
         break;
       
       case 3:
-        fflush(stdin);    
-        printf("Veuillez entrer le code de produit a aheter : ");
-        fgets(nom_prd,50,stdin);
-        printf("\nLa quantite a aheter : ");
-        scanf("%d",&qty_ach);
-        achat(prd , nom_prd , qty_ach);
+        achat(prd);
         break;
       
       case 4:
-        printf("\nDe quel recherche vous pouvez chercher \n");
-        printf("\n1.Par code");
-        printf("\n2.Par quantite");
-        printf("\n0.Retour a la menu");
-        do
-        {
-          printf("\n\nVotre choix : ");
-          scanf("%d",&rch);
-        } while (rch<0 && rch>3);
-        recherche(prd,rch);
+        recherche(prd);
         break;
       case 5:
         printf("\nLes produits ayant une quantite inferieur a 3 : \n");
